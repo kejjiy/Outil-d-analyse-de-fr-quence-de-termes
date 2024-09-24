b83e005cb4a4d4dbed875d7aaf34096701cf6199
kejjiy <145474682+kejjiy@users.noreply.github.com>
2024-09-24 16:41:12 +0000

    Retest

--- recherche_terme.py
+++ recherche_terme.py
@@ -145,7 +145,7 @@ def download_results(results):
     )
 
 def main():
-    st.title('Analyse de la Fréquence des Termes dans les PV')
+    st.title('Analyse de la Fréquence des Termes dans les PV du Conseil Constitutionnel')
 
     # Téléchargement des fichiers
     uploaded_files = st.file_uploader(
